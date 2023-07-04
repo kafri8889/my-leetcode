8b775a3c1d045b4bfcf3f7213aa757b81ea020c1
Anaf Naufalian <kafri8889@gmail.com>
2023-07-04 20:36:58 +0700

    Time: 253 ms (7.32%), Space: 39 MB (46.34%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.kt
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.kt
@@ -10,18 +10,26 @@
 class Solution {
     fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
     var child: ListNode? = null
-    val reversed = head?.getVals()?.split(k)?.mapIndexed { i, v ->
-        if (v.size >= k) v.reversed() else v
+    val list = head?.getVals()?.split(k)?.map {
+        // jika size listnya lebih besar atau sama dengan k, reverse
+        // k = 2, list = [[1,2],[3,4],[5]], menjadi
+        // [[2,1],[4,3],[5]]
+        if (it.size >= k) it.reversed() else it
     }
-    reversed?.let {
+    
+    // Ubah int list menjadi ListNode 
+    list?.let {
         for (num in it.flatten().reversed()) {
             if (child == null) child = ListNode(num)
             else child = ListNode(num).apply { next = child }
         }
     }
+    
     return child
 }
 
+// Split int list
+// list = [1,2,3,4,5], k = 2  ==>  [[1,2],[3,4],[5]]
 fun <T> List<T>.split(k: Int): List<List<T>> {
     return arrayListOf<List<T>>().apply {
         val temp = arrayListOf<T>()
@@ -35,6 +43,9 @@ fun <T> List<T>.split(k: Int): List<List<T>> {
     }
 }
 
+// Convert list node menjadi int list
+// input: ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, null)))))
+// output: [1,2,3,4,5]
 fun ListNode.getVals(): List<Int> {
     return arrayListOf<Int>().apply {
         var currHead: ListNode? = this@getVals
