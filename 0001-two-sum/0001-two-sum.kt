710e4f1a955f2fc27806bc448cd640a49676ac14
Anaf Naufalian <kafri8889@gmail.com>
2023-08-07 20:45:28 +0700

    Time: 207 ms (75.88%), Space: 38.2 MB (38.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -1,12 +1,11 @@
 class Solution {
- fun twoSum(nums: IntArray, target: Int): IntArray {
-    val map = hashMapOf<Int, Int>()
-    for (i in nums.indices) {
-        if (map.containsKey(nums[i])) {
-            return intArrayOf(map[nums[i]]!!, i)
-        } else map[target - nums[i]] = i
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    hashMapOf<Int, Int>().apply {
+        for (i in nums.indices) {
+            if (containsKey(nums[i])) return intArrayOf(get(nums[i])!!, i)
+            else set(target - nums[i], i)
+        }
     }
-
     return intArrayOf()
 }
 }
