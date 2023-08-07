801ae1d1bdb0d309e1caac9999789217b2dfc5cd
Anaf Naufalian <kafri8889@gmail.com>
2023-08-07 12:32:09 +0700

    Time: 271 ms (88.46%), Space: 42.9 MB (100%) - LeetHub

---
+++ 1572-subrectangle-queries/1572-subrectangle-queries.kt
@@ -0,0 +1,19 @@
+class SubrectangleQueries(private val rectangle: Array<IntArray>) {
+
+    fun updateSubrectangle(row1: Int, col1: Int, row2: Int, col2: Int, newValue: Int) {
+        for (i in row1..row2) {
+            for (j in col1..col2) {
+                rectangle[i][j] = newValue
+            }
+        }
+    }
+
+    fun getValue(row: Int, col: Int): Int = rectangle[row][col]
+}
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * var obj = SubrectangleQueries(rectangle)
+ * obj.updateSubrectangle(row1,col1,row2,col2,newValue)
+ * var param_2 = obj.getValue(row,col)
+ */
