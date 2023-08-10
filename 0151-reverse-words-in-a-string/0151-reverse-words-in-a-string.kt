0c36a0aa3ba4f4e621d0df3b7a65175b1bdf6451
Anaf Naufalian <kafri8889@gmail.com>
2023-08-10 11:13:35 +0700

    Time: 218 ms (32.08%), Space: 37.9 MB (32.08%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
@@ -4,8 +4,11 @@ fun reverseWords(s: String): String {
     val split = s.split(" ")
     for (i in split.lastIndex downTo 0) {
         val s = split[i]
-        if (s.isNotBlank()) res += "$s "
+        if (s.isNotBlank()) {
+            res += s
+            if (i != 0) res += " "
+        }
     }
-    return res.replace("^[ \\t]+|[ \\t]+\$".toRegex(), "")
+    return if (res.last().isWhitespace()) res.substring(0, res.lastIndex) else res
 }
 }
