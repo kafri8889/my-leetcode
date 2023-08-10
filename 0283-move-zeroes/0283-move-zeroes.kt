352da8de354d6aff81397de19cb530f2921dd707
Anaf Naufalian <kafri8889@gmail.com>
2023-08-10 14:49:11 +0700

    Time: 654 ms (5.05%), Space: 41 MB (25.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.kt
+++ 0283-move-zeroes/0283-move-zeroes.kt
@@ -1,22 +1,22 @@
 class Solution {
     fun moveZeroes(nums: IntArray): Unit {
-        if (nums.size == 1) return
-        
-        while (true) {
-        var p2 = 1
-        var arrayChanged = false
-        for (p1 in nums.indices) {
-            if (nums[p1] < Math.abs(nums[p2]) && nums[p1] == 0) {
-                // Swap
-                nums[p1] = nums[p2].also { nums[p2] = nums[p1] }
-                arrayChanged = true
-            }
-            if (p2 == nums.lastIndex) break
-            p2++
+    if(nums.size == 1) return
+    var p1 = 0
+    var p2 = 1
+    var movedZero = 0
+    val totalZero = nums.count { it == 0 }
+    while (totalZero > movedZero) {
+        if (nums[p1] < Math.abs(nums[p2]) && nums[p1] == 0) {
+            // Swap
+            nums[p1] = nums[p2].also { nums[p2] = nums[p1] }
         }
-
-        // Jika tidak ada perubahan urutan, break
-        if (!arrayChanged) break
+        if (p2 >= nums.lastIndex) {
+            p2 = 1.also { p1 = 0 }
+            movedZero++
+            continue
         }
+        p2++
+        p1++
+    }
     }
 }
