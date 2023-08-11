5fe09f3c64d97e77d3f3f7e456ea78055244f779
Anaf Naufalian <kafri8889@gmail.com>
2023-08-11 21:29:10 +0700

    Time: 380 ms (46.51%), Space: 41.7 MB (55.23%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.kt
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.kt
@@ -1,7 +1,7 @@
 class Solution {
 fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
-    val diff1 = mutableSetOf<Int>()
-    val diff2 = mutableSetOf<Int>()
+    val diff1 = hashSetOf<Int>()
+    val diff2 = hashSetOf<Int>()
     for (num in nums1) if (num !in nums2) diff1.add(num)
     for (num in nums2) if (num !in nums1) diff2.add(num)
     return listOf(diff1.toList(), diff2.toList())
