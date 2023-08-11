8dcb6b0b26a2bf09c82e429c74f72c9c7a8dabcf
Anaf Naufalian <kafri8889@gmail.com>
2023-08-11 21:45:46 +0700

    Time: 159 ms (73.49%), Space: 34.3 MB (85.54%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.kt
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.kt
@@ -2,14 +2,8 @@ class Solution {
 fun uniqueOccurrences(arr: IntArray): Boolean {
     val map = hashMapOf<Int, Int>()
     val set = hashSetOf<Int>()
-    for (n in arr) {
-        map.putIfAbsent(n, 0)
-        map[n] = map[n]!! + 1
-    }
-    for (v in map.values) {
-        if (set.contains(v)) return false
-        set.add(v)
-    }
+    for (n in arr) if (map.containsKey(n)) map[n] = map[n]!! + 1 else map[n] = 1
+    for (v in map.values) if (set.contains(v)) return false else set.add(v)
     return true
 }
 }
