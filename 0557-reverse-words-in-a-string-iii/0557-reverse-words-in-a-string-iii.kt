361346831c907ec8294beb9a2fb3bc152aecbb30
Anaf Naufalian <kafri8889@gmail.com>
2023-08-14 22:27:15 +0700

    Time: 342 ms (11.32%), Space: 39 MB (18.87%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.kt
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.kt
@@ -1,13 +1,13 @@
 class Solution {
 fun reverseWords(s: String): String {
-    val ms = " $s "
+    val ms = " $s"
     var news = ""
-    var j = 0
-    for (i in ms.indices) {
+    var temp = ""
+    for (i in ms.lastIndex downTo 0) {
         if (ms[i].isWhitespace()) {
-            for (k in i - 1 downTo j) news += ms[k]
-            j = i
-        }
+            news = "$temp $news"
+            temp = ""
+        } else temp += ms[i]
     }
     return news.trim()
 }
