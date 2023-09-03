c3b1413cbf44f678d539addba1ad63170bd06f62
Anaf Naufalian <kafri8889@gmail.com>
2023-09-03 20:08:31 +0700

    Time: 217 ms (75%), Space: 38.5 MB (70%) - LeetHub

--- 1241-decompress-run-length-encoded-list/1241-decompress-run-length-encoded-list.kt
+++ 1241-decompress-run-length-encoded-list/1241-decompress-run-length-encoded-list.kt
@@ -1,13 +1,7 @@
 class Solution {
 fun decompressRLElist(nums: IntArray): IntArray {
     return arrayListOf<Int>().apply {
-        var i = 0
-        var j = 1
-        for (z in 0 until nums.size / 2) {
-            for (y in 0 until nums[i]) add(nums[j])
-            i += 2
-            j += 2
-        }
+        for (i in nums.indices step 2) repeat(nums[i]) { add(nums[i + 1]) }
     }.toIntArray()
 }
 }
