fcaf9db6bc100dbd140b5d66d7e61314d188caa1
Anaf Naufalian <kafri8889@gmail.com>
2023-09-04 22:16:59 +0700

    Time: 187 ms (91.67%), Space: 37.5 MB (54.17%) - LeetHub

--- 2304-cells-in-a-range-on-an-excel-sheet/2304-cells-in-a-range-on-an-excel-sheet.kt
+++ 2304-cells-in-a-range-on-an-excel-sheet/2304-cells-in-a-range-on-an-excel-sheet.kt
@@ -1,9 +1,9 @@
 class Solution {
 fun cellsInRange(s: String): List<String> {
+    val strs = Array(1 + s[3].code - s[0].code) { s[0] + it }
+    val nums = Array(1 + s[4].digitToInt() - s[1].digitToInt()) { s[1].digitToInt() + it }
     return arrayListOf<String>().apply {
-        for (c in s[0]..s[3]) {
-            for (n in s[1].digitToInt()..s[4].digitToInt()) add("$c$n")
-        }
+        for (c in strs) for (n in nums) add("$c$n")
     }
 }
 }
