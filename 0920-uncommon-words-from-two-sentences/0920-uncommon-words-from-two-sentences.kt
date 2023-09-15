7327c38cc17ed2d3c5f6ed766ca33640eed98b24
Anaf Naufalian <kafri8889@gmail.com>
2023-09-15 13:40:36 +0700

    Time: 161 ms (100%), Space: 37.6 MB (57.89%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.kt
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.kt
@@ -1,9 +1,20 @@
 class Solution {
 fun uncommonFromSentences(s1: String, s2: String): Array<String> {
-    return arrayListOf<String>().apply {
-        Hashtable<String, Int>().apply {
-            for (s in ("$s1 $s2").split(' ')) if (containsKey(s)) set(s, get(s)!! + 1) else put(s, 1)
-        }.let { for ((k, v) in it) if (v == 1) add(k) }
-    }.toTypedArray()
+    val arr = arrayListOf<String>()
+    val hash = Hashtable<String, Int>()
+    val combinedString = ("$s1 $s2").split(' ') // ["this", "apple" "is", "sweet", "this", "apple"....]
+
+    for (s in combinedString) {
+        if (hash.containsKey(s)) hash[s] = hash[s]!! + 1 
+        else hash[s] = 1
+    }
+    
+    for ((k, v) in hash) {
+        if (v == 1) {
+            arr.add(k)
+        }
+    }
+
+    return arr.toTypedArray()
 }
 }
