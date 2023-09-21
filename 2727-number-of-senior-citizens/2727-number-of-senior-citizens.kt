ec8136f0463a7b9e0d03cae6827d6a99a41ce89f
Anaf Naufalian <kafri8889@gmail.com>
2023-09-21 14:55:41 +0700

    Time: 180 ms (55.88%), Space: 37.5 MB (23.53%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.kt
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.kt
@@ -1,7 +1,7 @@
 class Solution {
 fun countSeniors(details: Array<String>): Int {
     var count = 0
-    for (detail in details) if (detail.slice(11..12).toInt() > 60) count++
+    for (detail in details) if ("${detail[11]}${detail[12]}".toInt() > 60) count++
     return count
 }
 }
