957db76fbb88742fc784e4a6b8eccb0d036b26af
Anaf Naufalian <kafri8889@gmail.com>
2023-09-30 22:57:32 +0700

    Time: 405 ms (21.67%), Space: 39.3 MB (23.33%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.kt
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.kt
@@ -1,14 +1,15 @@
 class Solution {
 fun countSymmetricIntegers(low: Int, high: Int): Int {
-    var count = 0
-    for (i in low..high) {
-        if (intLength(i) % 2 != 0) continue
-        val s = i.toString()
-        val firstHalf = s.substring(0 until s.length / 2).sumOf { it.digitToInt() }
-        val lastHalf = s.substring(s.length / 2 until s.length).sumOf { it.digitToInt() }
-        if (firstHalf == lastHalf) count++
+    return (low..high).fold(0) { acc, i ->
+        if (intLength(i) % 2 == 0) {
+            val s = i.toString()
+            val firstHalf = s.substring(0 until s.length / 2).sumOf { it.digitToInt() }
+            val lastHalf = s.substring(s.length / 2 until s.length).sumOf { it.digitToInt() }
+            if (firstHalf == lastHalf) return@fold acc + 1
+        }
+
+        acc
     }
-    return count
 }
 
 /**
