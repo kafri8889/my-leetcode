4e0c42174bdf9dc6a05981c883fc427f84aab1bb
Anaf Naufalian <kafri8889@gmail.com>
2023-10-01 21:31:45 +0700

    Time: 143 ms (12.5%), Space: 33.7 MB (25%) - LeetHub

--- 1965-sum-of-digits-in-base-k/1965-sum-of-digits-in-base-k.kt
+++ 1965-sum-of-digits-in-base-k/1965-sum-of-digits-in-base-k.kt
@@ -1,3 +1,11 @@
 class Solution {
-    fun sumBase(n: Int, k: Int): Int = n.toString(k).sumOf { it.digitToInt() }
+fun sumBase(n: Int, k: Int): Int {
+    return arrayListOf<Int>().apply {
+        var mn = n
+        while (mn > 0) {
+            add(if (size == 0) 0 else lastIndex, mn % k)
+            mn /= k
+        }
+    }.sum()
+}
 }
