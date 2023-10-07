a373106156d35bb8539dcb52e047d2158cfc5b80
Anaf Naufalian <kafri8889@gmail.com>
2023-10-07 18:20:04 +0700

    Time: 170 ms (31.43%), Space: 34.9 MB (73.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.kt
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.kt
@@ -1,16 +1,9 @@
 class Solution {
 fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
-    var i = 0
-    var j = 0
-
     // for loop from 0 until m x n matrix size
-    for (x in 0 until matrix.size * matrix[0].size) {
-        println("i, j: $i, $j")
-        if (matrix[i][j] == target) return true
-        if (j == matrix[0].lastIndex) {
-            i++
-            j = 0
-        } else j++
+    for (n in 0 until matrix.size * matrix[0].size) {
+        // Convert 1D array index to 2D array index
+        if (matrix[n / matrix[0].size][n % matrix[0].size] == target) return true
     }
 
     return false
