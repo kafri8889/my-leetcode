d88c4299f2e480cf91f0b115a5dbd95a1ec4bef6
Anaf Naufalian <kafri8889@gmail.com>
2023-10-23 15:27:54 +0700

    Time: 335 ms (26.67%), Space: 48.1 MB (53.33%) - LeetHub

--- 2447-merge-similar-items/2447-merge-similar-items.kt
+++ 2447-merge-similar-items/2447-merge-similar-items.kt
@@ -2,12 +2,7 @@ class Solution {
 fun mergeSimilarItems(items1: Array<IntArray>, items2: Array<IntArray>): List<List<Int>> {
     val tab = Hashtable<Int, Int>()
 
-    for (item in items1) {
-        if (tab.containsKey(item[0])) tab[item[0]] = tab[item[0]]!! + item[1]
-        else tab[item[0]] = item[1]
-    }
-
-    for (item in items2) {
+    for (item in items1 + items2) {
         if (tab.containsKey(item[0])) tab[item[0]] = tab[item[0]]!! + item[1]
         else tab[item[0]] = item[1]
     }
