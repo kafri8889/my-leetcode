1776b0b239949db980c76023239280be3d0f7d47
Anaf Naufalian <kafri8889@gmail.com>
2023-11-03 19:59:12 +0700

    Time: 166 ms (50%), Space: 37.2 MB (75%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.kt
+++ 2580-circular-sentence/2580-circular-sentence.kt
@@ -1,17 +1,20 @@
 class Solution {
 fun isCircularSentence(sentence: String): Boolean {
-    var firstChar: Char
-    var lastChar: Char
     for (i in 1 until sentence.length) {
         if (sentence.getOrNull(i + 1) == null) {
-            lastChar = sentence[i]
-            if (sentence[0] != lastChar) return false
-        }
-        if (sentence.getOrNull(i + 1)?.isWhitespace() == true) {
-            firstChar = sentence[i]
-            lastChar = sentence[i + 2]
+            // Di kondisi ini, artinya pengecekan di kondisi if dibawah sudah berhasil
+            // atau `sentence` hanya memiliki 1 kata
+            // Berarti `firstChar` kita ganti menjadi karakter pertama dari kalimat ini
 
-            if (firstChar != lastChar) return false
+            // Cek apakah karakter pertama dan terakhir equals dari kalimat ini
+            if (sentence[0] != sentence.last()) return false
+        }
+
+        if (sentence.getOrNull(i + 1)?.isWhitespace() == true) {
+            // sentence[i] adl karakter terakhir dari kata saat ini
+            // sentence[i + 1] adl karakter white space (spasi)
+            // sentence[i + 2] adl karakter pertama dari kata selanjutnya
+            if (sentence[i] != sentence[i + 2]) return false
         }
     }
 
