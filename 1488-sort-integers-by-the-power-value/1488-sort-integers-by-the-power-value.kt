f187c8e53552ddc72cef5c31012ed05dfe562456
Anaf Naufalian <kafri8889@gmail.com>
2023-11-09 22:55:53 +0700

    Time: 305 ms (20%), Space: 39.8 MB (60%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.kt
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.kt
@@ -1,9 +1,7 @@
 class Solution {
 fun getKth(lo: Int, hi: Int, k: Int): Int {
-    return arrayListOf<Pair<Int, Int>>().apply {
+    return Array(hi - lo + 1) { 1 to 1 }.apply {
         for ((i, num) in (lo..hi).withIndex()) {
-            add(num to 0)
-
             var x = num
             while (x != 1) {
                 if (x % 2 == 0) x /= 2 else x = 3 * x + 1
