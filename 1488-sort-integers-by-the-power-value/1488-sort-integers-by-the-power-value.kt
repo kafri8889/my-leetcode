3659d8cd3767e572a05b41abc339fc75cd75a75a
Anaf Naufalian <kafri8889@gmail.com>
2023-11-09 22:59:43 +0700

    Time: 316 ms (20%), Space: 37.6 MB (80%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.kt
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.kt
@@ -1,6 +1,6 @@
 class Solution {
 fun getKth(lo: Int, hi: Int, k: Int): Int {
-    return Array(hi - lo + 1) { 1 to 1 }.apply {
+    val arr = Array(hi - lo + 1) { 1 to 1 }.apply {
         for ((i, num) in (lo..hi).withIndex()) {
             var x = num
             while (x != 1) {
@@ -8,6 +8,13 @@ fun getKth(lo: Int, hi: Int, k: Int): Int {
                 set(i, num to get(i).second + 1)
             }
         }
-    }.sortedBy { it.second }[k - 1].first
+    }
+
+    Arrays.sort(arr) { o1, o2 -> 
+        if (o1.second == o2.second) o1.first.compareTo(o2.first) 
+        else o1.second.compareTo(o2.second)
+    }
+
+    return arr[k - 1].first
 }
 }
