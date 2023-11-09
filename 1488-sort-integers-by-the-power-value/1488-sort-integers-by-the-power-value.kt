2bc8578fbdd6591849bec9902ffa0a8a430cb6e7
Anaf Naufalian <kafri8889@gmail.com>
2023-11-09 22:49:05 +0700

    Time: 404 ms (20%), Space: 40.6 MB (40%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.kt
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.kt
@@ -1,19 +1,15 @@
 class Solution {
 fun getKth(lo: Int, hi: Int, k: Int): Int {
-    
-    val map = arrayListOf<Pair<Int, Int>>().apply {
-        for (i in lo..hi) {
-            var x = i
-            var step = 0
+    return arrayListOf<Pair<Int, Int>>().apply {
+        for ((i, num) in (lo..hi).withIndex()) {
+            add(num to 0)
+
+            var x = num
             while (x != 1) {
                 if (x % 2 == 0) x /= 2 else x = 3 * x + 1
-                step++
+                set(i, num to get(i).second + 1)
             }
-
-            add(i to step)
         }
-    }
-
-    return map.sortedBy { it.second }.also { println(it) }[k - 1].first
+    }.sortedBy { it.second }[k - 1].first
 }
 }
