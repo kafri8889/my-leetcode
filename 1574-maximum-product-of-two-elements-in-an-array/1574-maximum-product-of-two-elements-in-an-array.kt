b97abc588478312f14286d40cffb99ee928b2d82
Anaf Naufalian <kafri8889@gmail.com>
2025-11-04 13:46:18 +0700

    Time: 27 ms (8.33%), Space: 46.4 MB (25%) - LeetHub

--- 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.kt
+++ 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.kt
@@ -1,7 +1,24 @@
 class Solution {
     fun maxProduct(nums: IntArray): Int {
-        nums.sort()
+        bubbleSort(nums)
 
         return (nums[nums.lastIndex] - 1) * (nums[nums.lastIndex - 1] - 1)
     }
+
+    private fun swap(arr: IntArray, i: Int, j: Int) {
+    arr[i] = arr[j].also { arr[j] = arr[i] }
+}
+
+private fun shouldSwap(n1: Int, n2: Int, ascending: Boolean): Boolean = if (ascending) n1 < n2 else n1 > n2
+
+fun bubbleSort(arr: IntArray, ascending: Boolean = true) {
+    if (arr.size <= 1) return
+
+    for (x in arr.indices) {
+        for (i in arr.indices) {
+            if (i == arr.size - 1) break
+            if (shouldSwap(arr[i + 1], arr[i], ascending)) swap(arr, i, i + 1)
+        }
+    }
+}
 }
