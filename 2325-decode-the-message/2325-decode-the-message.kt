3bde1c2265fbc408c74f73db963ae7437cec73e0
Anaf Naufalian <kafri8889@gmail.com>
2026-01-27 10:37:05 +0700

    Time: 21 ms (20%), Space: 45.6 MB (0%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.kt
@@ -0,0 +1,21 @@
+class Solution {
+fun decodeMessage(key: String, message: String): String {
+    val hashMap = HashMap<Char, Char>() // key, act
+    var cCode = 'a'.code - 1
+    var decoded = ""
+
+    hashMap.put(' ', ' ')
+
+    for (c in key) {
+        if (hashMap.containsKey(c) || c.isWhitespace()) continue
+
+        hashMap[c] = (++cCode).toChar()
+    }
+
+    println(hashMap)
+
+    for (c in message) decoded += hashMap[c]!!
+
+    return decoded
+}
+}
